9efc08d844cdd2ecd69a7c77865a6a1618a8bafd
farah <f.khaled2243@nu.edu.eg>
2025-12-31 13:02:32 +0200

    Add Railway deployment config

--- lockspot_backend/settings.py
+++ lockspot_backend/settings.py
@@ -31,6 +31,8 @@ else:
         'https://*.ngrok-free.app',
         'https://*.ngrok.io',
         'https://*.onrender.com',
+        'https://*.railway.app',
+        'https://*.up.railway.app',
         'http://localhost:8000',
         'http://127.0.0.1:8000',
     ]
@@ -200,6 +202,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
